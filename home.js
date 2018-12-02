6cea85db582157963c181ce2a92e4b3ef3bc9b7e
benwalley <benwalleyorigami@gmail.com>
2018-12-01 19:02:48 -0600

    move links to random positions. Just for fun

--- home.js
+++ home.js
@@ -5,7 +5,26 @@ var links = [
 ];
 
 function init() {
-	console.log("working")
+	createLinks(links)
+	positionLinks()
 }
 
-$(window).onload(init);
+function createLinks(links){
+	for (var i = 0; i < links.length; i++) {
+		var currentLink = $("<a class = 'floatingLink' href = '/" + links[i] + "'>"+ links[i] + "</a>")
+		$(".main").append(currentLink)
+	}
+}
+
+function positionLinks() {
+	var allLinks = $(".floatingLink");
+	for (var i = 0; i < allLinks.length; i++) {
+		var x = Math.random()*(window.innerWidth - 100) + "px"
+		var y = Math.random()*window.innerHeight + "px"
+		console.log(y)
+		allLinks[i].style.left = x
+		allLinks[i].style.top = y
+	}
+}
+
+$(document).ready(init);
